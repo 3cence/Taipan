7594172cd426dece9ff6aeff714b3d23450c0f7c
Emma <emmaalderson24@gmail.com>
2022-07-05 10:59:38 -0400

    Getting rid of build files

--- build/CMakeFiles/Taipan.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libTaipan.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ecence/Documents/C++/Taipan
-# Build directory: /home/ecence/Documents/C++/Taipan/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
